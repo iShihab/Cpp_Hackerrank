07ce20f4cd1e7021d71d7b5e3b8daae6974adb64
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-19 02:26:26 +0600

    finishing touch

--- ConditionalStatements.cpp
+++ ConditionalStatements.cpp
@@ -1,2 +1,53 @@
 //url: https://www.hackerrank.com/challenges/c-tutorial-conditional-if-else/problem?h_r=next-challenge&h_v=zen
 
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a;
+    cin >> a;
+    if (a == 1)
+    {
+        cout << "one" << endl;
+    }
+    else if (a == 2)
+    {
+        cout << "two" << endl;
+    }
+    else if (a == 3)
+    {
+        cout << "three" << endl;
+    }
+    else if (a == 4)
+    {
+        cout << "four" << endl;
+    }
+    else if (a == 5)
+    {
+        cout << "five" << endl;
+    }
+    else if (a == 6)
+    {
+        cout << "six" << endl;
+    }
+    else if (a == 7)
+    {
+        cout << "seven" << endl;
+    }
+    else if (a == 8)
+    {
+        cout << "eight" << endl;
+    }
+    else if (a == 9)
+    {
+        cout << "nine" << endl;
+    }
+    else
+    {
+        cout << "Greater than 9";
+    }
+
+    return 0;
+}
