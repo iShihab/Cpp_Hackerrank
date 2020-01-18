60e10fcc321bb22dd5c8f266fb0be6ac57a75508
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-19 01:54:10 +0600

    final code
    added long long int  instead of int. beacause input sample has lengthy input

--- BasicDataTypes.cpp
+++ BasicDataTypes.cpp
@@ -7,19 +7,19 @@ using namespace std;
 int main()
 {
     int a;
-    //long b;
+    long long int b;
     char c;
     float d;  //floating point value should be correct up to 3 decimal places
     double e; //the double to 9 decimal places.
     // scanf("%d,%ld,%ch,%f,%lf", a, b, c, d, e);
     cin >> a;
-    // cin >> b;
+    cin >> b;
     cin >> c;
     cin >> d;
     cin >> e;
 
     cout << a << endl;
-    // cout << b << endl;
+    cout << b << endl;
     cout << c << endl;
     cout << fixed << setprecision(3) << d << endl;
     cout << fixed << setprecision(9) << e << endl;
