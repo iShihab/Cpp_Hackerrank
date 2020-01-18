7d871a47ec7a151e420030f4dbfabddf3be86a53
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-19 00:45:29 +0600

    taking input using cin
    eliminating scanf

--- BasicDataTypes.cpp
+++ BasicDataTypes.cpp
@@ -11,7 +11,13 @@ int main()
     char c;
     float d;  //floating point value should be correct up to 3 decimal places
     double e; //the double to 9 decimal places.
-    scanf("%d,%ld,%ch,%f,%lf", a, b, c, d, e);
+    // scanf("%d,%ld,%ch,%f,%lf", a, b, c, d, e);
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    cin >> e;
+
     cout << a << endl;
     cout << b << endl;
     cout << c << endl;
