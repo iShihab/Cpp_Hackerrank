a9ac513bcf4f9864d7ed0c201683e5a5868abb27
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-19 04:26:41 +0600

    Update ForLoop.cpp

--- ForLoop.cpp
+++ ForLoop.cpp
@@ -1,1 +1,61 @@
 // url: https://www.hackerrank.com/challenges/c-tutorial-for-loop/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a, b;
+    cin >> a;
+    cin >> b;
+
+    for (int i = a; i <= b; i++)
+    {
+        if (i == 1)
+        {
+            cout << "one" << endl;
+        }
+        else if (i == 2)
+        {
+            cout << "two" << endl;
+        }
+        else if (i == 3)
+        {
+            cout << "three" << endl;
+        }
+        else if (i == 4)
+        {
+            cout << "four" << endl;
+        }
+        else if (i == 5)
+        {
+            cout << "five" << endl;
+        }
+        else if (i == 6)
+        {
+            cout << "six" << endl;
+        }
+        else if (i == 7)
+        {
+            cout << "seven" << endl;
+        }
+        else if (i == 8)
+        {
+            cout << "eight" << endl;
+        }
+        else if (i == 9)
+        {
+            cout << "nine" << endl;
+        }
+        else if (i > 9 && i % 2 == 0)
+        {
+            cout << "even";
+        }
+        else if (i > 9 && i % 2 != 0)
+        {
+            cout << "odd";
+        }
+    }
+
+    return 0;
+}
