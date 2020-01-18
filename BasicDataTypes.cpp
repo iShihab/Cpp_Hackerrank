63e73e61a16d88d48490ebabd6b3d77500fa1be4
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-19 01:47:32 +0600

    setprecision and fixed
    must have to add fixed before precision inorder to print particular decimal places

--- BasicDataTypes.cpp
+++ BasicDataTypes.cpp
@@ -7,22 +7,22 @@ using namespace std;
 int main()
 {
     int a;
-    long b;
+    //long b;
     char c;
     float d;  //floating point value should be correct up to 3 decimal places
     double e; //the double to 9 decimal places.
     // scanf("%d,%ld,%ch,%f,%lf", a, b, c, d, e);
     cin >> a;
-    cin >> b;
+    // cin >> b;
     cin >> c;
     cin >> d;
     cin >> e;
 
     cout << a << endl;
-    cout << b << endl;
+    // cout << b << endl;
     cout << c << endl;
-    cout << setprecision(3) << d << endl;
-    cout << setprecision(9) << e << endl;
+    cout << fixed << setprecision(3) << d << endl;
+    cout << fixed << setprecision(9) << e << endl;
 
     return 0;
 }
