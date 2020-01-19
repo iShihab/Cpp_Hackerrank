ffb308881c7c17dbcebdfbeca9f38c9f21bce28f
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-20 04:49:51 +0600

    Finished

--- ArraysIntroduction.cpp
+++ ArraysIntroduction.cpp
@@ -1,1 +1,24 @@
 // url: https://www.hackerrank.com/challenges/arrays-introduction/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
