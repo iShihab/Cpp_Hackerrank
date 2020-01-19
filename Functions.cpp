bbe3ed2c6f64bd0488620c68761de48658e2c410
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-19 19:30:18 +0600

    max_of_four
    function created as required

--- Functions.cpp
+++ Functions.cpp
@@ -4,6 +4,30 @@
 
 using namespace std;
 
+int max_of_four(int a, int b, int c, int d)
+{
+    if (a > b && a > c && a > d)
+    {
+        return a;
+    }
+    else if (b > a && b > c && b > d)
+    {
+        return b;
+    }
+    else if (c > a && c > b && c > d)
+    {
+        return c;
+    }
+    else if (d > a && d > b && d > c)
+    {
+        return d;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     int a, b, c, d;
