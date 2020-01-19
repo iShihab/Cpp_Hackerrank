2778b1877c222de6172cf8f1f50d7b6f96cc2131
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-19 19:28:54 +0600

    Main function
    To take input

--- Functions.cpp
+++ Functions.cpp
@@ -1,1 +1,16 @@
 // url : https://www.hackerrank.com/challenges/c-tutorial-functions/problem?h_r=next-challenge&h_v=zen
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a, b, c, d;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+
+    return 0;
+}
