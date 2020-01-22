6ef548d54ff4802616c814516bd423c2870043a2
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-23 03:50:14 +0600

    without null in last

--- test.cpp
+++ test.cpp
@@ -1,24 +1,43 @@
-#include <stdio.h>
-#include <cstdlib>
+#include <iostream>
 
-void update(int *a, int *b)
+using namespace std;
+
+int main(int argc, char *argv[])
 {
-    // Complete this function
-    int sum, sub;
-    sum = *a + *b;
-    sub = *a - *b;
-    *a = sum;
-    *b = abs(sub);
-}
+    int n;
+    int q;
+    cin >> n >> q;
 
-int main()
-{
-    int a, b;
-    int *pa = &a, *pb = &b;
+    // Create an array of pointers to integer arrays
+    // (i.e., an array of variable-length arrays)
+    int **outer = new int *[n];
 
-    scanf("%d %d", &a, &b);
-    update(pa, pb);
-    printf("%d\n%d", a, b);
+    // Fill each index of 'outer' with a variable-length array
+    for (int i = 0; i < n; i++)
+    {
+        int k;
+        cin >> k;
+        // Create an array of length 'k' at index 'i'
+        outer[i] = new int[k];
+
+        // Fill each cell in the 'inner' variable-length array
+        for (int j = 0; j < k; j++)
+        {
+            cin >> outer[i][j];
+        }
+    }
+
+    // Perform queries:
+    while (q-- > 0)
+    {
+        int outer_index;
+        int inner_index;
+        cin >> outer_index >> inner_index;
+
+        // Find the variable-length array located at outer_index
+        // and print the value of the element at inner_index.
+        cout << outer[outer_index][inner_index] << endl;
+    }
 
     return 0;
 }
